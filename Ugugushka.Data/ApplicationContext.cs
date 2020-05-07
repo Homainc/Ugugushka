8f7236cb06465bd78ec8c86de240643d9dd7b9b0
Philip <spritefok@gmail.com>
2020-05-07 20:29:50 +0300

    + added EF Core context and context factory

---
+++ Ugugushka.Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ugugushka.Data.Models;
+
+namespace Ugugushka.Data
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Partition> Partitions { get; set; }
+        public DbSet<Toy> Toys { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            if (Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+                Database.Migrate();
+            else
+                Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new PartitionConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ToyConfiguration());
+;        }
+    }
+}
---
+++ Ugugushka.Data/ApplicationContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Ugugushka.Data
+{
+    internal class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+
+            var cfg = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = cfg.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationContext(optionsBuilder.Options);
+        }
+    }
+}
