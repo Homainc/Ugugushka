ecad5984e6873a5fcec1b91403353fcc6745b486
Philip <spritefok@gmail.com>
2020-05-07 21:24:33 +0300

    + added repository interface and abstract repository

---
+++ Ugugushka.Data/Code/Abstractions/AbstractRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Ugugushka.Data.Code.Interfaces;
+
+namespace Ugugushka.Data.Code.Abstractions
+{
+    internal abstract class AbstractRepository<TItem> : IRepository<TItem> where TItem : class
+    {
+        protected readonly ApplicationContext Db;
+        protected readonly CancellationToken CancellationToken;
+
+        public AbstractRepository(ApplicationContext db, IHttpContextAccessor httpContextAccessor)
+        {
+            Db = db;
+            CancellationToken = httpContextAccessor.HttpContext.RequestAborted;
+        }
+
+        public void Create(TItem item) => Db.Entry(item).State = EntityState.Added;
+
+        public void Update(TItem item) => Db.Entry(item).State = EntityState.Modified;
+
+        public void Delete(TItem item) => Db.Entry(item).State = EntityState.Deleted;
+    }
+}
---
+++ Ugugushka.Data/Code/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Ugugushka.Data.Code.Interfaces
+{
+    interface IRepository<in TItem> where TItem : class
+    {
+        void Create(TItem item);
+        void Update(TItem item);
+        void Delete(TItem item);
+    }
+}
